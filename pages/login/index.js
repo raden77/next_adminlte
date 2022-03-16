c0a530d1906c86726ce638e4bc89ccf7f2dbc373
raden <radenpapang147@gmail.com>
2022-03-16 17:07:33 +0700

    membuat starter template

---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from '../../styles/Home.module.css'
+export default function index() {
+  return <div className={style.container}>
+            <button className={style.btn}>
+                Login
+            </button>
+        </div>
+  
+}
